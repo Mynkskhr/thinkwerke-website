d4dabf24b462d0b84e54938f8f0b6c6cdb1ddb4e
Mynkskhr <31133119+Mynkskhr@users.noreply.github.com>
2025-12-27 15:26:27 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const repo = "REPO_NAME";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
 };
 
 module.exports = nextConfig;
